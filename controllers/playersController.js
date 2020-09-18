38d6c28f4c6ed1e7dac3bbac9dd852565259fb6e
bonasunu <bonaventurasunu@gmail.com>
2020-09-19 01:16:38 +0700

    implement post and delete operation via sequelize

--- controllers/playersController.js
+++ controllers/playersController.js
@@ -1,24 +1,50 @@
+const { v4: uuidv4 } = require('uuid')
 const db = require('../db/models')
 
 const getAllPlayers = async (req, res) => {
   const players = await db.Player.findAll({
     include: [db.PlayerBio, db.PlayerHistory],
   })
-  res.render('players', { players })
+  res.json({ players })
 }
 
-const getAllPlayersBio = async (req, res) => {
-  const players = await db.PlayerBio.findAll()
-  res.status(200).json(players)
+const createPlayer = async (req, res) => {
+  const player = req.body
+  const uuid = uuidv4()
+  await db.Player.create(
+    {
+      id: uuid,
+      username: player.username,
+      email: player.email,
+      PlayerBio: {
+        uid: uuid,
+      },
+      PlayerHistories: [
+        {
+          uid: uuid,
+        },
+      ],
+    },
+    {
+      include: [db.PlayerBio, db.PlayerHistory],
+    }
+  )
+
+  res.json({ status: 'Created' })
 }
 
-const getAllPlayersLogs = async (req, res) => {
-  const players = await db.PlayerHistory.findAll()
-  res.status(200).json(players)
+const deletePlayer = async (req, res) => {
+  await db.Player.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+
+  res.json({ status: 'Deleted' })
 }
 
 module.exports = {
   getAllPlayers,
-  getAllPlayersBio,
-  getAllPlayersLogs,
+  createPlayer,
+  deletePlayer,
 }
--- routes/players.js
+++ routes/players.js
@@ -2,10 +2,12 @@ const Router = require('express-promise-router')
 const router = new Router()
 const controller = require('../controllers/playersController')
 
-router.get('/', controller.getAllPlayers)
-
-router.get('/log', controller.getAllPlayersLogs)
-
-router.get('/bio', controller.getAllPlayersBio)
+router.get('/players', controller.getAllPlayers)
+// TODO add player route
+router.post('/players/create', controller.createPlayer)
+router.delete('/players/delete/:id', controller.deletePlayer) //TODO fix cascade delete, edit migration
+// TODO update player data
+// TODO update player bio
+// TODO update player log
 
 module.exports = router
